af2f16a48eaf37e9bb9ce2ad274319eba6cbdb49
tapasjoshi1991 <tapasjoshi1991@gmail.com>
2022-09-22 12:53:13 +0530

    Initial commit

---
+++ src/main/java/com/wellsfargo/gcp/springgcpdemo/SpringGcpDemoApplication.java
@@ -0,0 +1,13 @@
+package com.wellsfargo.gcp.springgcpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGcpDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGcpDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wellsfargo/gcp/springgcpdemo/SpringGcpDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellsfargo.gcp.springgcpdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGcpDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
